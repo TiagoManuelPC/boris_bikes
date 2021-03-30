fb15f5afb933b60429a677c7316622178de6681c
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-03-30 17:35:18 +0100

    end of step 9

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+require './lib/docking_station'
+
+describe DockingStation do 
+    it { is_expected.to respond_to :release_bike }
+    #it 'responds_to realease_bike' do 
+    ##docking_station = (DockingStation.new)
+        ###expect(docking_station).to respond_to(:release_bike) 
+        
+
+    
+  end
